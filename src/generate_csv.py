b9e26c59ad29cb1550d18bcf09103f01667ee296
Wadie Assal <wadie.assal@true.nl>
2019-06-15 09:48:20 +0200

    code cleanup

--- src/generate_csv.py
+++ src/generate_csv.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import os
 
+# Used for sample dataset
 exit_please = 0
 
 # Dataset directory
@@ -9,41 +10,41 @@ directory = "../dataset/hackathon-for-good-2019_TWB-challenge_files"
 # Generated dataset
 hackforgood_dataset_csv = "filename, sourcelang, targetlang, sourcetext, targettext\n"
 
+# Loop through al the dirs
 for filename in os.listdir(os.path.abspath(directory)):
     if filename.endswith(".sdlxliff"):
+        
+        # Get file and parse XML
         file_path = os.path.join(directory, filename)
-
         doc_tree = ET.parse(file_path)
 
-        # CSV collumns
-
+        # Get the source and target languages
         file_nodes = doc_tree.findall(".//{urn:oasis:names:tc:xliff:document:1.2}file")
-
         sourcelang = file_nodes[0].attrib.get('source-language')
-
         targetlang = file_nodes[0].attrib.get('target-language')
 
+        # Get the source text
         sourcetext = ""
-        
         sourcetext_list = doc_tree.findall(".//{urn:oasis:names:tc:xliff:document:1.2}source")
 
         for elem in sourcetext_list:
                 for elem in elem.iter():
                         if elem.text is not None: 
-                                sourcetext = sourcetext + " " + elem.text
+                                sourcetext += " " + elem.text
         
+        # Get the target text
         targettext = ""
-
         targettext_list = doc_tree.findall(".//{urn:oasis:names:tc:xliff:document:1.2}target")
 
         for elem in targettext_list:
                 for elem in elem.iter():
                         if elem.text is not None: 
-                                targettext = targettext + " " + elem.text
+                                targettext += " " + elem.text
 
-        hackforgood_dataset_csv = hackforgood_dataset_csv + "\"{}\", {}, {}, \"{}\", \"{}\"\n".format(filename, sourcelang, targetlang, sourcetext, targettext) 
+        hackforgood_dataset_csv += "\"{}\", {}, {}, \"{}\", \"{}\"\n".format(filename, sourcelang, targetlang, sourcetext, targettext) 
         
-        exit_please = exit_please + 1
+        #Only generate a sample dataset
+        exit_please += 1
         if exit_please > 500:
                 hackforgood_dataset_csv_file = open("../dataset/hackforgood_dataset.csv", "w")
                 hackforgood_dataset_csv_file.write(hackforgood_dataset_csv)
